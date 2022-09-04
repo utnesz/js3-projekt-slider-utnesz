50c21d829fb3aaefce67bcb48798b8aae4294d60
utnesz <90829509+utnesz@users.noreply.github.com>
2022-09-04 19:53:31 +0200

    nyűglődés

--- main.js
+++ main.js
@@ -17,15 +17,19 @@ img_main = document.querySelector(".inner_image");
 
 img_main.setAttribute("src", pictures[0].src);
 
-/*const dotsTemplate = `<img class="hop" src="./pictures/hop.png" alt=""></img>`;
-dots.innerHTML += (dotsTemplate);
-dotsTemplate.forEach((pictures, ) => {
-    
-});*/
+
+
+const dotsTemplate = `<img class="hop" src="./pictures/hop.png" alt=""></img>`;
+
+pictures.forEach(() => dots.innerHTML = (dotsTemplate));
+
+
+
 
 arrow_right.addEventListener("click", () => {
     picId === pictures.length-1 ? picId = 0 : picId++;
     steps(picId);
+  
 });
 
 arrow_left.addEventListener("click", () => {
@@ -37,8 +41,11 @@ const steps = (picId) => {
     img_main.setAttribute("src", pictures[picId].src);
     counter.innerHTML = `${picId + 1}/${pictures.length}`;
     tag.innerHTML = (pictures[picId].tag);
+
 }
 
-
-
+setInterval(() => {
+    picId === pictures.length - 1 ? picId = 0 : picId++;
+    steps(picId);
+}, 3500);
 
